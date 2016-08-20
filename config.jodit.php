3a281f3325ab3bae3d8c1c4c95e848976c984961
Chupurnov Valeriy <skoder@ya.ru>
2016-08-21 03:28:37 +0500

    New version 2.5.13. Read more http://xdsoft.net/jodit/doc/index.html#2.5.13

--- config.jodit.php
+++ config.jodit.php
@@ -3,16 +3,23 @@ define('_JEXEC', 1);
 define('DS', DIRECTORY_SEPARATOR );
 define('JPATH_BASE', realpath(realpath(__DIR__).'/../../../')); // replace to valid path
 
+$saveconfig = array_merge(array(), $config); //save $config because in framework.php it will overwride
+
 require_once JPATH_BASE . '/includes/defines.php';
 require_once JPATH_BASE . '/includes/framework.php';
 require_once JPATH_BASE . DS . 'libraries' . DS . 'joomla' . DS . 'factory.php';
 
-$config = array(
-    'root' => JPATH_BASE.'/images/',
-    'baseurl' => '/images/',
-    'extensions' => array('jpg', 'png', 'gif', 'jpeg'),
-    'debug' => false,
-);
+$config = $saveconfig;
+
+$plugin = JPluginHelper::getPlugin('editors', 'jodit');
+$params = new JRegistry($plugin->params);
+
+$config['root'] = JPATH_BASE.'/images/';
+$config['baseurl'] = '/images/';
+$config['extensions'] = array('jpg', 'png', 'gif', 'jpeg');
+$config['debug'] = $params->get('debug', $config['debug']);
+$config['createThumb'] = $params->get('create_thumb', $config['createThumb']);
+$config['thumbFolderName'] = $params->get('thumb_folder_name', $config['thumbFolderName']);
 
 function JoditCheckPermissions() {
     $app = JFactory::getApplication(isset($_REQUEST['isadmin']) && (int)$_REQUEST['isadmin'] ? 'administrator' : 'site');
