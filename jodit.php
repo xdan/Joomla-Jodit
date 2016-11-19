398a13ff36f2275cd7767d045bd4529c79774078
Chupurnov Valeriy <skoder@ya.ru>
2016-11-20 03:31:06 +0500

    New version 2.5.45. Read more http://xdsoft.net/jodit/doc/index.html#2.5.45

--- jodit.php
+++ jodit.php
@@ -37,7 +37,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '2.5.44';
+	protected $version = '2.5.45';
 
 	/**
 	 * Initialises the Editor.
