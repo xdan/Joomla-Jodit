65550e49cf6ccdad47e00617d70ff69b64f8a3cf
Chupurnov Valeriy <skoder@ya.ru>
2016-11-29 13:44:47 +0500

    New version 2.5.53. Read more http://xdsoft.net/jodit/doc/index.html#2.5.53

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Joomla-Jodit
-Joomla Jodit editor Plugin v.2.5.52
+Joomla Jodit editor Plugin v.2.5.53
--- jodit.php
+++ jodit.php
@@ -37,7 +37,7 @@ class plgEditorJodit extends JPlugin {
 	 * @since  3.2
 	 */
 	protected $app = null;
-	protected $version = '2.5.52';
+	protected $version = '2.5.53';
 
 	/**
 	 * Initialises the Editor.
